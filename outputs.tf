ffe46cdf29976837ff747bc466eedea6b2059971
Vasil Vasilev <vasilev@palantir.com>
2025-01-07 11:17:28 -0800

    Define module outputs

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "rds_master_password_secret_arn" {
-  description = "The name of the secret storing the master user password"
-  value       = aws_db_instance.retool_postgres.master_user_secret[0].secret_arn
+output "rds_master_password_secret_path" {
+  description = "The path of the secret storing the master user password"
+  value       = element(split(":", aws_db_instance.retool_postgres.master_user_secret[0].secret_arn), length(split(":", aws_db_instance.retool_postgres.master_user_secret[0].secret_arn)) - 1)
 }
 
 output "rds_hostname" {
